3064a4af6cdbb2725500c3c1a474e58ca02ab373
afrozamitu <afrozamitulive@gmail.com>
2023-07-22 21:08:40 +0600

    navbar and banner added

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaArrowRight, FaBrain } from 'react-icons/fa';
+// import './Banner.css'
+
+const Banner = () => {
+    return (
+        <div className='hat-art-bg'>
+            <div className='grid md:flex items-center justify-between md:w-10/12 md:mx-auto mx-5 py-20'>
+                <div className='space-y-3 pt-14 md:pt-0'>
+                    <h1 className='text-3xl md:text-5xl'>Your Gateway to Higher <br />Education</h1>
+                    <p className='text-lg'>Explore, Compare, and Reserve Your Seat Online - Your Journey <br />to Success Starts Here!</p>
+                    <div>
+                        <Link to='/' className='flex items-center justify-center gap-2 bg-orange-400 py-2 px-5 w-40 rounded-full  text-white'>Learn More <FaArrowRight /></Link>
+                    </div>
+                </div>
+                <div className='flex justify-center items-center relative md:pt-0 pt-10'>
+                    <img className='w-full md:w-[450px]' src="https://i.ibb.co/cLPXfG8/banner-girl.png" alt="" />
+
+                    <span className='flex items-center justify-center gap-1 md:py-2 py-1 md:px-3 px-2 bg-white rounded shadow-md shadow-orange-400 absolute md:top-16 top-24 md:-left-10 -left-2'>
+                        <p>Successfully Applied</p>
+                        <p className='bg-green-500 w-6 h-6 md:w-8 md:h-8 rounded-full flex items-center justify-center font-bold text-xl text-white'>✓</p>
+                    </span>
+
+                    
+                    <span className='absolute bottom-10 md:bottom-10 md:-left-28 -left-2 md:py-2 py-1 md:px-3 px-2 bg-white rounded shadow-md shadow-orange-400'>
+                        <p className='flex items-start gap-3'>
+                            <img className='w-8 h-8' src="https://i.ibb.co/hVrSLgs/Vector-1.png" alt="" />
+                            Fantastic college resource,<br/>easy seat booking! A must<br/> visit for aspiring students!</p>
+                    </span>
+
+                    <span className='absolute top-18 md:top-48 md:-right-8 -right-2 md:py-2 py-1 md:px-3 px-2 bg-white rounded shadow-md shadow-orange-400'>
+                        <p className='flex items-center gap-1'><FaBrain className='text-orange-400' />Creative Institutes</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/Pages/Shared/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
+
+const Header = () => {
+
+    const navlinks = <>
+        <Link to='/'>Home</Link>
+        <Link to='/colleges'>Colleges</Link>
+        <Link to='/mycollege'>My College</Link>
+    </>
+
+    return (
+        <div className='bg-white'>
+            <div className="navbar fixed z-10 md:px-24 shadow-md">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 gap-4">
+                            <Link to='/userprofile' className="">My Profile</Link>
+
+                            {navlinks}
+
+                            <Link className="bg-orange-400 py-2 px-5 rounded-full  text-white">Login</Link>
+                        </ul>
+                    </div>
+                    <span className='flex items-center'>
+                        <img className='md:w-24 w-14' src="https://i.ibb.co/V38wWgR/logo.png" alt="" />
+                        <Link className="md:text-5xl text-3xl font-semibold cursive-font">Academix</Link>
+                    </span>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 gap-8 text-base font-semibold">
+                        {navlinks}
+                    </ul>
+                </div>
+                <div className="navbar-end">
+                    <span className='flex items-center gap-3'>
+                        <span className='relative'>
+                            <input type="text" placeholder="Search. . ." className="input w-full max-w-xs rounded-full border border-slate-200" />
+                            <FaSearch className='absolute top-[6px] right-2 bg-orange-400 text-white w-9 h-9 p-2 rounded-full cursor-pointer' />
+                        </span>
+                        <img className='w-10 h-10 hidden md:block' src="https://simpleicon.com/wp-content/uploads/user1.png" alt="" />
+                        <Link className="bg-orange-400 py-2 px-5 rounded-full  text-white hidden md:block">Login</Link>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
